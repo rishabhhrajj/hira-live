9ffeaffd99d7afa8dd626252ff89fa2bee46a830
rishabhhrajj <rishabhraj6965@gmail.com>
2026-06-15 17:01:30 +0530

    Added wedding themed live stream UI

--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,4 @@
 document
-  .getElementById("broadcastBtn")
-  .addEventListener("click", () => {
-
-    window.location.href =
-      "/admin.html";
-  });
-
-document
   .getElementById("watchBtn")
   .addEventListener("click", () => {
 
